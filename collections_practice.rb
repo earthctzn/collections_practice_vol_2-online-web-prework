0fb35de21d420b1cc723220093ea5f3f2510faba
Caleb Einarr <earthctzn1@gmail.com>
2019-04-13 03:18:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,11 @@
 def begins_with_r(array)
   array.each do |words|
-    words.each do |x|
-      binding.pry
-      if x.start_with?("r") == false
+      if words.start_with?("r") == false
       return false
       elsif x.start_with?("r") == true
       return true
+    else
+      puts "invalid array"
       end
     end
   end
