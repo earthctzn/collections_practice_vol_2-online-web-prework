f5b0c53675bfd7c7becbcddb8bbf3fddcfd49a41
Caleb Einarr <earthctzn1@gmail.com>
2019-04-13 03:12:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,10 @@
 def begins_with_r(array)
   array.each do |words|
-    words.each do |x| x.start_with?("r") == false
-    return false
-    elsif x.start_with?("r") == true
-    return true
+    words.each do |x| 
+      if x.start_with?("r") == false
+      return false
+      elsif x.start_with?("r") == true
+      return true
+      end
     end
-  end
 end
