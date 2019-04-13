c70d707476c42d27af2b7f03fa0fdd4fcaca5146
Caleb Einarr <earthctzn1@gmail.com>
2019-04-13 03:16:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,9 @@
+require "pry"
+
 def begins_with_r(array)
   array.each do |words|
-    words.each do |x| 
+    words.each do |x|
+      binding.pry
       if x.start_with?("r") == false
       return false
       elsif x.start_with?("r") == true
